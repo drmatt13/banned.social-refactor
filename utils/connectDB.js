bdb2da3dd3c5a6364ff0d0b3af29cece4a0f7f18
drmatt13 <58347724+drmatt13@users.noreply.github.com>
2022-04-06 04:18:09 -0400

    4/6

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -4,15 +4,15 @@ import mongoose from "mongoose";
 const connectDB = (handler) => async (req, res) => {
   // Use current db connection
   if (mongoose.connections[0].readyState) {
-    console.log("Using current db connection".bold.green);
+    // console.log("Using current db connection".bold.green);
     return handler(req, res);
   }
   // Use new db connection
-  mongoose.connect(process.env.MONGO_URL, {
+  await mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   });
-  console.log("Connected to DB".bold.green);
+  // console.log("Connected to DB".bold.green);
   return handler(req, res);
 };
 
