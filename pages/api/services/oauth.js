7406e5ac193656292aa317b21dfb31747eb0e4af
drmatt13 <58347724+drmatt13@users.noreply.github.com>
2022-04-06 23:16:15 -0400

    4/6

--- pages/api/services/oauth.js
+++ pages/api/services/oauth.js
@@ -11,26 +11,30 @@ export default connectDB(async (req, res) => {
       token: req.body.sessionToken,
       secret: process.env.NEXTAUTH_SECRET,
     });
-    console.log(decodedSessionToken);
-    // check if email exists in database
-    let user = await User.findOne({ email: decodedSessionToken.email });
-    if (!user) {
-      // if email does not exist, create user
-      user = await User.create({
-        firstName: "undefined",
-        lastName: "undefined",
-        email: decodedSessionToken.email,
-        username: decodedSessionToken.name,
-        password: "undefined",
-      });
-    }
-    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
-    user = user.toObject();
-    delete user.password;
+    // console.log(decodedSessionToken);
+    // // check if email exists in database
+    // let user = await User.findOne({ email: decodedSessionToken.email });
+    // if (!user) {
+    //   // if email does not exist, create user
+    //   user = await User.create({
+    //     firstName: "undefined",
+    //     lastName: "undefined",
+    //     email: decodedSessionToken.email,
+    //     username: decodedSessionToken.name,
+    //     password: "undefined",
+    //   });
+    // }
+    // const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+    // user = user.toObject();
+    // delete user.password;
+    // return res.json({
+    //   success: true,
+    //   token,
+    //   user,
+    // });
     return res.json({
       success: true,
-      token,
-      user,
+      test: decodedSessionToken,
     });
   } catch (err) {
     console.error(err.message);
